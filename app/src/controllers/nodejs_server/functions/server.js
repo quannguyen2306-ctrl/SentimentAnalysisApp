9aa06b862cac9c9589783f3112c26a62f9bae8f4
Nguyen Hoang Quan <quanhoangnguyen2306@gmail.com>
2024-04-21 18:29:46 +0700

    add video route

--- app/src/controllers/nodejs_server/functions/server.js
+++ app/src/controllers/nodejs_server/functions/server.js
@@ -1,47 +1,71 @@
-import express from 'express'; 
-import grpc from '@grpc/grpc-js'; 
-import protoLoader from '@grpc/proto-loader'; 
+import express from 'express';
+import grpc from '@grpc/grpc-js';
+import protoLoader from '@grpc/proto-loader';
 import bodyParser from 'body-parser';
 
 import Comment from "./services/Comment.services.js";
+import Video from "./services/Video.services.js";
 
-
-const app = express(); 
-app.use(bodyParser.json()); 
+const app = express();
+app.use(bodyParser.json());
 
 // Load gRPC protobuf definition
 const PORT = process.env.PORT || 8000;
-const PROTO_PATH = "/Users/nguyenhoangquan/Documents/sental_app/SentimentAnalysisTool/app/src/controllers/proto/comment_list.proto"; 
+const PROTO_PATH = "/Users/nguyenhoangquan/Documents/gate_sental/SENTAL/sental_app/app/src/controllers/proto/comment_list.proto";
 const packageDefinition = protoLoader.loadSync(PROTO_PATH);
-const { CommentsPrediction } = grpc.loadPackageDefinition(packageDefinition); 
+const { CommentsPrediction } = grpc.loadPackageDefinition(packageDefinition);
 
 // Create gRPC client 
-const pythonClient = new CommentsPrediction('localhost:50051', grpc.credentials.createInsecure()); 
+const pythonClient = new CommentsPrediction('localhost:50051', grpc.credentials.createInsecure());
 
-
-function predictComments(comments) { 
-    return new Promise((resolve, reject) => { 
+function predictComments(comments) {
+    return new Promise((resolve, reject) => {
         pythonClient.predict(comments, (err, response) => {
             if (err) {
                 reject(err);
             } else {
-                resolve(response.list);
+                console.log(response)
+                resolve(response.result);
             }
-        });    
+        });
     })
 }
 
-app.get('/predict', async (req, res) => { 
+app.get('/predict', async (req, res) => {
     const link = req.query.link
     const commentClass = new Comment(link)
     const commentsListRequest = await commentClass.getComments();
     try {
-        const predictions = await predictComments({list: commentsListRequest});
+        const predictions = await predictComments({ list: commentsListRequest });
+        const exp = 3;
+        const dp = Math.pow(10,exp);
+        for (let key in predictions) {
+            predictions[key] = Math.round(predictions[key] * dp) / dp; // round to <dp> decimal places
+        }
+        let sum = Object.values(predictions).reduce((acc, curr) => acc + curr, 0);
+        predictions.NEU += 1 - sum;
+        predictions.NEU = Math.round(predictions.NEU * dp) / dp
         res.json({ predictions });
     } catch (error) {
         console.error('Error:', error);
         res.status(500).json({ error: 'Internal Server Error' });
     }
+})
+
+app.get('/video', async (req, res) => { 
+    const link = req.query.link 
+    const video = new Video(link); 
+    video.getInfo()
+        .then(videoInfo => {
+            console.log('Title:', videoInfo.title);
+            console.log('Author:', videoInfo.author);
+            console.log('Thumbnail URL:', videoInfo.thumbnail);
+            res.json(videoInfo)
+        })
+        .catch(error => {
+            console.error(error.message);
+            res.status(500).json({ error: 'Internal Server Error' });
+        });
 })
 
 // Start the server
---
+++ app/src/controllers/nodejs_server/functions/services/Video.services.js
@@ -0,0 +1,61 @@
+class YouTubeVideo {
+    constructor(videoUrl) {
+        this.videoUrl = videoUrl;
+        this.videoId = this._extractVideoId(videoUrl);
+        this.apiKey = "AIzaSyBV9j-2DheWwvUkvBgu80SwizV6UEjr274";
+    }
+
+    _extractVideoId(videoUrl) {
+        let videoId;
+        if (videoUrl.includes('youtube.com')) {
+            videoId = videoUrl.split('v=')[1];
+            if (videoId.includes('&')) {
+                videoId = videoId.split('&')[0];
+            }
+        } else if (videoUrl.includes('youtu.be')) {
+            videoId = videoUrl.split('/').pop().split("?")[0];
+        } else {
+            throw new Error('Invalid YouTube URL');
+        }
+        console.log(videoId)
+        return videoId;
+    }
+
+    async getInfo() {
+        const apiUrl = `https://www.googleapis.com/youtube/v3/videos?id=${this.videoId}&key=${this.apiKey}&part=snippet`;
+        try {
+            const response = await fetch(apiUrl);
+            const data = await response.json();
+            if (data.items && data.items.length > 0) {
+                const videoInfo = data.items[0].snippet;
+                const title = videoInfo.title;
+                const author = videoInfo.channelTitle;
+                const thumbnail = videoInfo.thumbnails.default.url;
+                return {
+                    title: title,
+                    author: author,
+                    thumbnail: thumbnail
+                };
+            } else {
+                throw new Error('Video not found or API request failed');
+            }
+        } catch (error) {
+            throw new Error('Failed to fetch video info');
+        }
+    }
+}
+
+// Example usage
+// const youtubeVideo = new YouTubeVideo('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+// youtubeVideo.getInfo()
+//     .then(videoInfo => {
+//         console.log('Title:', videoInfo.title);
+//         console.log('Author:', videoInfo.author);
+//         console.log('Thumbnail URL:', videoInfo.thumbnail);
+//     })
+//     .catch(error => {
+//         console.error(error.message);
+//     });
+
+
+export default YouTubeVideo
